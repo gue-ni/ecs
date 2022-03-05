77ff2bcc25943a615b6e462d9a749eb35ae10ad7
Jakob <jakob.g.maier@gmail.com>
2022-03-05 15:40:51 +0100

    update

--- example/game.ts
+++ example/game.ts
@@ -15,6 +15,9 @@ bulletSprite.src = "assets/bullet.png";
 const treeSprite = new Image();
 treeSprite.src = "assets/tree.png";
 
+const boxSprite = new Image();
+boxSprite.src = "assets/box.png";
+
 let windowOffset = 0;
 let windowCenter = 0;
 windowCenter = canvas.width / 2;
@@ -34,27 +37,52 @@ class Vector extends ECS.Component {
 	}
 }
 
+/**
+ *       _
+ *	  a	 |
+ *       |  b
+ * | --- X --- |
+ *    d  |
+ *       |c
+ *       _
+ */
 class BoundingBox extends ECS.Component {
 	active: boolean;
+	centerX: number;
+	centerY: number;
 
-	constructor(active: boolean = false) {
+	a: number;
+	b: number;
+	c: number;
+	d: number;
+
+	constructor(a: number, b: number, c: number, d: number, active: boolean = false) {
 		super();
 		this.active = active;
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
+	}
+
+	set_center(x: number, y: number): void {
+		this.centerX = x;
+		this.centerY = y;
 	}
 
 	get minX() {
-		return 0;
+		return this.centerX - this.d;
 	}
 
 	get maxX() {
-		return 0;
+		return this.centerX + this.b;
 	}
 
 	get minY() {
-		return 0;
+		return this.centerY - this.a;
 	}
 	get maxY() {
-		return 0;
+		return this.centerY + this.c;
 	}
 }
 
@@ -62,18 +90,24 @@ class Weapons extends ECS.Component {}
 
 class Primary extends ECS.Component {}
 
+class Static extends ECS.Component {}
+
+class Dynamic extends ECS.Component {}
+
 class Velocity extends Vector {}
 
 class Position extends ECS.Component {
 	_x: number;
 	_y: number;
 	clamp: boolean;
+	changed: boolean;
 
 	constructor(x: number, y: number, clamp: boolean = true) {
 		super();
 		this._x = x;
 		this._y = y;
 		this.clamp = clamp;
+		this.changed = true;
 	}
 
 	get x() {
@@ -85,10 +119,12 @@ class Position extends ECS.Component {
 	}
 
 	set x(x) {
+		this.changed = true;
 		this._x = x;
 	}
 
 	set y(y) {
+		this.changed = true;
 		this._y = y;
 	}
 }
@@ -107,22 +143,26 @@ class Sprite extends ECS.Component {
 		image: HTMLImageElement,
 		width: number,
 		height: number,
-		frameX: number,
-		frameY: number,
-		maxFrameX: number,
-		maxFrameY: number
+		frameX: number = 0,
+		frameY: number = 0,
+		maxFrameX: number = 1,
+		maxFrameY: number = 1
 	) {
 		super();
 		this.image = image;
 		this.width = width;
 		this.height = height;
-		this.frameX = frameX || 0;
-		this.frameY = frameY || 0;
-		this.maxFrameX = maxFrameX || 1;
-		this.maxFrameY = maxFrameY || 1;
+		this.frameX = frameX;
+		this.frameY = frameY;
+		this.maxFrameX = maxFrameX;
+		this.maxFrameY = maxFrameY;
 		this.animate = false;
 	}
 }
+
+class Explosive extends ECS.Component {}
+
+class Destructible extends ECS.Component {}
 
 class Input extends ECS.Component {
 	pressed: any;
@@ -246,6 +286,8 @@ class WeaponSystem extends ECS.System {
 			projectile.addComponent(new Position(position.x + 6, position.y - 5, false));
 			projectile.addComponent(new Velocity(300, -50));
 			projectile.addComponent(new Sprite(grenadeSprite, 9, 3, 0, 0, 1, 1));
+			projectile.addComponent(new BoundingBox(2, 2, 2, 2, true));
+			projectile.addComponent(new Explosive());
 			params.ecs.addEntity(projectile);
 		}
 
@@ -254,8 +296,21 @@ class WeaponSystem extends ECS.System {
 			projectile.addComponent(new Position(position.x + 6, position.y - 5, false));
 			projectile.addComponent(new Velocity(500, -10));
 			projectile.addComponent(new Sprite(bulletSprite, 9, 3, 0, 0, 1, 1));
+			projectile.addComponent(new BoundingBox(2, 2, 2, 2, true));
+			projectile.addComponent(new Explosive());
 			params.ecs.addEntity(projectile);
 		}
+	}
+}
+
+class PositionChangeSystem extends ECS.System {
+	constructor() {
+		super([Position]);
+	}
+
+	updateEntity(entity: ECS.Entity, params: ECS.UpdateParams): void {
+		const position = entity.getComponent(Position) as Position;
+		position.changed = false;
 	}
 }
 
@@ -328,10 +383,12 @@ class SpriteSystem extends ECS.System {
 
 class SpatialHashGrid {
 	_grid: Map<string, ECS.Entity[]>;
+	_lastPos: Map<string, number[]>;
 	_gridsize: number;
 
 	constructor(gridsize: number) {
 		this._grid = new Map();
+		this._lastPos = new Map();
 		this._gridsize = gridsize;
 	}
 
@@ -339,21 +396,51 @@ class SpatialHashGrid {
 		return [Math.floor(x / this._gridsize), Math.floor(y / this._gridsize)];
 	}
 
-	insert(entity: ECS.Entity) {
-		let box = entity.getComponent(BoundingBox) as BoundingBox;
-		if (!box) new Error("Entity must have a bounding box");
+	remove(entity: ECS.Entity): void {
+		if (!this._lastPos.has(entity.id)) return;
 
-		let [minX, minY] = this.hash(box.minX, box.minY);
-		let [maxX, maxY] = this.hash(box.maxX, box.maxY);
+		let [minX, minY, maxX, maxY] = this._lastPos.get(entity.id);
+		this._lastPos.delete(entity.id);
 
 		for (let i = minX; i <= maxX; i++) {
 			for (let j = minY; j <= maxY; j++) {
 				const key = `${i}/${j}`;
 
 				if (this._grid.has(key)) {
-					let l = this._grid.get(key);
-					l.push(entity);
-					this._grid.set(key, l);
+					let cell = this._grid.get(key);
+					this._grid.set(
+						key,
+						cell.filter((item) => item != entity)
+					);
+				}
+			}
+		}
+	}
+
+	insert(entity: ECS.Entity): void {
+		let box = entity.getComponent(BoundingBox) as BoundingBox;
+		if (!box) new Error("Entity must have a bounding box");
+
+		let [minX, minY] = this.hash(box.minX, box.minY);
+		let [maxX, maxY] = this.hash(box.maxX, box.maxY);
+
+		if (this._lastPos.has(entity.id)) {
+			let [lastMinX, lastMinY, lastMaxX, lastMaxY] = this._lastPos.get(entity.id);
+			if (minX == lastMinX && minY == lastMinY && maxX == lastMaxX && maxY == lastMaxY) {
+				return;
+			}
+		}
+
+		this._lastPos.set(entity.id, [minX, minY, maxX, maxY]);
+
+		for (let i = minX; i <= maxX; i++) {
+			for (let j = minY; j <= maxY; j++) {
+				const key = `${i}/${j}`;
+
+				if (this._grid.has(key)) {
+					let list = this._grid.get(key);
+					list.push(entity);
+					this._grid.set(key, list);
 				} else {
 					this._grid.set(key, [entity]);
 				}
@@ -395,19 +482,64 @@ class CollisionSystem extends ECS.System {
 		this.sph = new SpatialHashGrid(16);
 	}
 
-	_intersect(a: BoundingBox, b: BoundingBox): boolean {
-		return a.minX <= b.maxX && a.maxX >= b.minX && a.minY <= b.maxY && a.maxY >= b.minY;
+	_intersection(a: BoundingBox, b: BoundingBox): number[] | null {
+		if (a.minX <= b.maxX && a.maxX >= b.minX && a.minY <= b.maxY && a.maxY >= b.minY) {
+			let d0: number;
+			let d1: number;
+
+			d0 = a.maxX - b.minX;
+			d1 = b.maxX - a.minX;
+			let x = d0 < d1 ? d0 : -d1;
+
+			d0 = a.maxY - b.minY;
+			d1 = b.maxY - a.minY;
+			let y = d0 < d1 ? d0 : -d1;
+
+			//console.log(d0, d1, a.maxY, a.minY, b.maxY, b.minY);
+
+			return [x, y];
+		} else {
+			return null;
+		}
 	}
 
 	updateEntity(entity: ECS.Entity, params: ECS.UpdateParams): void {
-
 		// update bounding box
+		let aabb = entity.getComponent(BoundingBox) as BoundingBox;
+		let position = entity.getComponent(Position) as Position;
+		let velocity = entity.getComponent(Position) as Velocity;
 
-		for (let possible of this.sph.possible_collisions(entity)) {
-			let a = entity.getComponent(BoundingBox) as BoundingBox;
-			let b = possible.getComponent(BoundingBox) as BoundingBox;
-			if (this._intersect(a, b)) {
-				console.log("intersection", entity, possible);
+		if (position.changed) {
+			this.sph.remove(entity);
+			aabb.set_center(position.x, position.y);
+			this.sph.insert(entity);
+		}
+
+		if (aabb.active) {
+			for (const possible of this.sph.possible_collisions(entity)) {
+				const possible_aabb = possible.getComponent(BoundingBox) as BoundingBox;
+
+				let intersection = this._intersection(aabb, possible_aabb);
+				if (intersection) {
+					//console.log("intersection", entity.id, possible.id, intersection);
+
+					if (entity.getComponent(Explosive) && possible.getComponent(Destructible)) {
+						console.log("explosion");
+						params.ecs.removeEntity(entity);
+					}
+
+					if (entity.getComponent(Dynamic) && possible.getComponent(Static)) {
+						console.log("static intersection", intersection)
+						let [x, y] = intersection;
+
+						if (Math.abs(x) < Math.abs(y)) {
+							position.x -= x;
+
+						} else {
+							position.y -= y;
+						}
+					}
+				}
 			}
 		}
 	}
@@ -426,21 +558,35 @@ ecs.addSystem(new MovementSystem());
 ecs.addSystem(new WeaponSystem());
 ecs.addSystem(new SpriteSystem());
 ecs.addSystem(new CollisionSystem());
+ecs.addSystem(new PositionChangeSystem());
 
 const player = new ECS.Entity();
 player.addComponent(new Velocity(0, 0));
 player.addComponent(new Input());
 player.addComponent(new Weapons());
+player.addComponent(new Dynamic());
 player.addComponent(new Primary());
 player.addComponent(new Position(windowCenter, canvas.height));
-player.addComponent(new Sprite(characterSprite, 16, 16, 0, 0, 1, 1));
+player.addComponent(new Sprite(characterSprite, 16, 16));
+player.addComponent(new BoundingBox(16, 8, 0, 8, true));
 ecs.addEntity(player);
 
 for (let i = 0; i < 10; i++) {
 	const tree = new ECS.Entity();
-	tree.addComponent(new Position(i * 50, canvas.height));
-	tree.addComponent(new Sprite(treeSprite, 16, 16, 0, 0, 1, 1));
+	tree.addComponent(new Position(i * 90, canvas.height));
+	tree.addComponent(new Sprite(treeSprite, 16, 16));
+	tree.addComponent(new BoundingBox(16, 8, 0, 8, false));
 	ecs.addEntity(tree);
+}
+
+{
+	const box = new ECS.Entity();
+	box.addComponent(new Position(100, canvas.height));
+	box.addComponent(new Sprite(boxSprite, 16, 16));
+	box.addComponent(new BoundingBox(16, 8, 0, 8, false));
+	box.addComponent(new Destructible());
+	box.addComponent(new Static());
+	ecs.addEntity(box);
 }
 
 let dt: number = 0;
