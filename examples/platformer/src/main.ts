b06369be5ff2de1f7753bcacbdce0ca9460a14cd
Jakob <jakob.g.maier@gmail.com>
2022-05-05 14:13:18 +0200

    fixed collison bug for now, quadtree query needs to be improved

--- examples/platformer/src/main.ts
+++ examples/platformer/src/main.ts
@@ -1,4 +1,4 @@
-import * as ECS from "../../../lib";
+import * as ECS from "../../../src";
 
 const canvas: HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement;
 const context: CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D;
@@ -31,8 +31,8 @@ class SpriteSystem extends ECS.System {
 	}
 
 	updateEntity(entity: ECS.Entity, params: ECS.UpdateParams): void {
-		const rect = entity.getComponent(Sprite) as Sprite;
-		const position = entity.getComponent(ECS.Position) as ECS.Position;
+		const rect = entity.getComponent<Sprite>(Sprite);
+		const position = entity.getComponent<ECS.Position>(ECS.Position);
 		params.context.strokeStyle = rect.color;
 		params.context.strokeRect(Math.round(position.x), Math.round(position.y), rect.w, rect.h);
 	}
@@ -44,9 +44,9 @@ class PhysicsSystem extends ECS.System {
 	}
 
 	updateEntity(entity: ECS.Entity, params: ECS.UpdateParams): void {
-		const sprite = entity.getComponent(Sprite) as Sprite;
-		const position = entity.getComponent(ECS.Position) as ECS.Position;
-		const velocity = entity.getComponent(ECS.Velocity) as ECS.Velocity;
+		const sprite = entity.getComponent<Sprite>(Sprite);
+		const position = entity.getComponent<ECS.Position>(ECS.Position);
+		const velocity = entity.getComponent<ECS.Position>(ECS.Velocity);
 
 		position.x = position.x + params.dt * velocity.x;
 		position.y = position.y + params.dt * velocity.y;
@@ -64,16 +64,16 @@ class PhysicsSystem extends ECS.System {
 
 class MovementSystem extends ECS.System {
 	constructor() {
-		super([ECS.Input, ECS.Velocity]);
+		super([ECS.Input, ECS.Velocity, ECS.Collider]);
 	}
 
 	updateEntity(entity: ECS.Entity, params: ECS.UpdateParams): void {
-		const input = entity.getComponent(ECS.Input) as ECS.Input;
-		const velocity = entity.getComponent(ECS.Velocity) as ECS.Velocity;
+		const input = entity.getComponent<ECS.Input>(ECS.Input);
+		const velocity = entity.getComponent<ECS.Velocity>(ECS.Velocity);
+		const collider = entity.getComponent<ECS.Collider>(ECS.Collider);
 
 		const SPEED = 200;
-		const JUMP = 300;
-
+		const JUMP = 350;
 
 		if (input.is_key_pressed("ArrowLeft")) {
 			velocity.x = -SPEED;
@@ -83,8 +83,9 @@ class MovementSystem extends ECS.System {
 			velocity.x = 0;
 		}
 
-		if (input.is_key_pressed("ArrowUp", 500)) {
+		if (input.is_key_pressed("ArrowUp", 100) && collider.south) {
 			velocity.y = -JUMP;
+			console.log("jump")
 		}
 	}
 }
@@ -99,36 +100,18 @@ ecs.addSystem(new SpriteSystem());
 const TILESIZE = 16;
 
 {
-	const entity = new ECS.Entity();
-
-	let s = new Sprite(TILESIZE, TILESIZE, "green");
-	let i = new ECS.Input();
-	let p = new ECS.Position(50, 50);
-	let v = new ECS.Velocity(0, 0);
-	let c = new ECS.Collider(TILESIZE, TILESIZE);
-
-	console.log(
-		"sprite",
-		ECS.getComponentType(s),
-		"input",
-		ECS.getComponentType(i),
-		"position",
-		ECS.getComponentType(p),
-		"velocity",
-		ECS.getComponentType(v),
-		"collision",
-		ECS.getComponentType(c)
+	const entity = new ECS.Entity().addComponents(
+		new Sprite(TILESIZE, TILESIZE, "green"),
+		new ECS.Input(),
+		new ECS.Position(50, 50),
+		new ECS.Velocity(0, 0),
+		new ECS.Collider(TILESIZE, TILESIZE)
 	);
 
-	entity.addComponent(s);
-	entity.addComponent(i);
-	entity.addComponent(p);
-	entity.addComponent(v);
-	entity.addComponent(c);
 	ecs.addEntity(entity);
 }
 
-let boxes = [
+const boxes = [
 	[0, 3],
 	[0, 4],
 	[0, 5],
@@ -147,19 +130,33 @@ let boxes = [
 	[8, 1],
 	[9, 1],
 	[10, 1],
+	[10, 2],
+	[10, 3],
+	[11, 1],
+	[12, 1],
+	[13, 1],
+	[14, 1],
+	[15, 1],
+	[16, 1],
+	[17, 1],
+	[18, 1],
+	[19, 1],
 	[13, 4],
 	[14, 4],
-	[15, 2],
 	[6, 6],
 	[7, 6],
 	[8, 6],
+	[19, 2],
+	[19, 3],
+	[19, 4],
 ];
 
 for (const [x, y] of boxes) {
-	const entity = new ECS.Entity();
-	entity.addComponent(new ECS.Position(x * TILESIZE, canvas.height - y * TILESIZE));
-	entity.addComponent(new Sprite(TILESIZE, TILESIZE, "green"));
-	entity.addComponent(new ECS.Collider(TILESIZE, TILESIZE));
+	const entity = new ECS.Entity().addComponents(
+		new ECS.Position(x * TILESIZE, canvas.height - y * TILESIZE),
+		new Sprite(TILESIZE, TILESIZE, "green"),
+		new ECS.Collider(TILESIZE, TILESIZE)
+	);
 	ecs.addEntity(entity);
 }
 
